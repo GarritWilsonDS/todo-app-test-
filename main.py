6f967078ed6f4763efc6847bb807b15877f46fbb
Garrit Wilson <garritwilson@Garrits-MacBook-Air.local>
2025-02-18 18:20:47 -0500

    changed runtime code for main.py

--- main.py
+++ main.py
@@ -12,7 +12,18 @@ def start_api():
     app.run(debug=True)
 
 if (__name__ == "__main__"):
-    start_cli()
 
-    
+    print("1. Start CLI")
+    print("2. Start API")
+
+    choice = input("Do you want to start the CLI or the API? (1 or 2): ")
+
+    if choice == "1":
+        start_cli()
+
+    elif choice == "2":
+        start_api()
+
+    else:
+        print("Invalid choice. Please try again.")  
     
